51f031070eada85cac1832a3b5bb1e5a3deae5d5
Daniel Ntege <danientege785@gmail.com>
2024-06-01 18:53:20 +0300

    improve on the  signup ui

--- client/src/layouts/authentication/sign-up/index.js
+++ client/src/layouts/authentication/sign-up/index.js
@@ -1,39 +1,23 @@
 import { useState } from "react";
-
-// react-router-dom components
 import { Link } from "react-router-dom";
 import axios from "axios";
-
-// @mui material components
 import Card from "@mui/material/Card";
-import Checkbox from "@mui/material/Checkbox";
-
-// Soft UI Dashboard React components
 import SoftBox from "../../../components/SoftBox";
 import SoftTypography from "../../../components/SoftTypography";
 import SoftInput from "../../../components/SoftInput";
 import SoftButton from "../../../components/SoftButton";
-
-// Authentication layout components
 import BasicLayout from "../../../layouts/authentication/components/BasicLayout";
 import Socials from "../../../layouts/authentication/components/Socials";
 import Separator from "../../../layouts/authentication/components/Separator";
-
-// Images
 import curved6 from "../../../assets/images/curved-images/curved14.jpg";
 
-function SignUp() {
-  // const [agreement, setAgreement] = useState(true);
-
-  // const handleSetAgreement = () => setAgreement(!agreement);
-
+const  SignUp = () => {
   const [form, setForm] = useState({ 
     firstName: "", 
     lastName: "", 
     email: "", 
     password: "",
   });
-
   const [errorMessage, setErrorMessage] = useState('');
 
   const handleChange = (e) => {
@@ -42,17 +26,10 @@ function SignUp() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // if (!agreement) {
-    //   setMessage("You must agree to the terms and conditions.");
-    //   return;
-    // }
-    // Perform email existence check
     try {
       const response = await axios.post("https://buweb.onrender.com/auth/register", form);
-      // const response = await axios.post("http://localhost:3001/register", form);
       console.log(response.data);
       setErrorMessage("User created successfully")
-      // setMessage(response.data.message);
       setForm({ firstName: '', lastName: '', email: '', password: '' });
     } catch (error) {
       setErrorMessage(error.response.data.error || 'Registration failed!');
@@ -62,7 +39,6 @@ function SignUp() {
   return (
     <BasicLayout
       title="Welcome to admin panel!"
-      // description="Use these awesome forms to login or create new account in your project for free."
       image={curved6}
     >
       <Card>
@@ -119,25 +95,6 @@ function SignUp() {
             <SoftBox 
               display="flex" 
               alignItems="center">
-              {/* <Checkbox checked={agreement} onChange={handleSetAgremment} />
-              <Checkbox checked={agreement} /> */}
-              {/* <SoftTypography
-                variant="button"
-                fontWeight="regular"
-                // onClick={handleSetAgremment}
-                sx={{ cursor: "poiner", userSelect: "none" }}
-              >
-                &nbsp;&nbsp;I agree the&nbsp;
-              </SoftTypography>
-              <SoftTypography
-                component="a"
-                href="#"
-                variant="button"
-                fontWeight="bold"
-                textGradient
-              >
-                Terms and Conditions
-              </SoftTypography> */}
             </SoftBox>
             <SoftBox 
               mt={4} 
