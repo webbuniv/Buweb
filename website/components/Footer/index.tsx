b20446bc9f2316681266a1babce10f727724be86
Daniel Ntege <danientege785@gmail.com>
2024-06-01 18:08:56 +0300

    commit

--- website/components/Footer/index.tsx
+++ website/components/Footer/index.tsx
@@ -209,10 +209,10 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="bg-primary/10 py-8">
+        <div className="py-8">
           <div className="container">
             <p className="text-center text-base bg-blue-600 text-body-color dark:text-white">
-            <a href="https://agrichub.net" rel="nofollow noopener">Bugema University</a>
+              AgricHub<a href="https://agrichub.net" rel="nofollow noopener">AgricHub Team</a>
             </p>
           </div>
         </div>
