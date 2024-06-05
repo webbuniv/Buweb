edc3001da554c56e34a2eb3a98f3abd83cdf1de1
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2024-06-05 11:40:42 -0700

    more work today

--- website/components/Features/index.tsx
+++ website/components/Features/index.tsx
@@ -2,10 +2,16 @@ import SectionTitle from "../Common/SectionTitle";
 import SingleFeature from "./SingleFeature";
 import Image from 'next/image';
 import featuresData from "./featuresData";
+import img from "../../public/images/features/life.jpg";
 import img1 from "../../public/images/features/bu.jpg";
 import img2 from "../../public/images/features/bu2.jpg";
+import { BiSolidPencil } from 'react-icons/bi';
+import './style.css';
+import React, { useState } from 'react';
 
 const Features = () => {
+  const [hoveredButton, setHoveredButton] = useState(null);
+
   return (
     <>
       <section
@@ -19,18 +25,51 @@ const Features = () => {
               title="For ambition that can take you anywhere, get your start at Bugema University"
               paragraph=""
               // center
-            />
+              />
+               <div className="grid grid-cols-2 gap-x-8 gap-y-14 md:grid-cols-1 lg:grid-cols-2">
+                <div>
+                  <button
+                    className="animated-button flex rounded-md bg-primary/60 py-1 px-3 md:py-2 md:px-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
+                    onMouseEnter={() => setHoveredButton('whyBugema')}
+                    onMouseLeave={() => setHoveredButton(null)}
+                  >
+                  <span className="icon-container">
+                    <BiSolidPencil className="text-xl mx-2" />
+                  </span>
+                  Why Bugema
+                 
+                  </button>
+                </div>
+               <div>
+                  <button
+                    className="animated-button flex rounded-md bg-primary/60 py-1 px-3 md:py-2 md:px-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
+                    onMouseEnter={() => setHoveredButton('studentLife')}
+                    onMouseLeave={() => setHoveredButton(null)}
+                  >
+                  <span className="icon-container">
+                    <BiSolidPencil className="text-xl mx-2" />
+                  </span>
+                  Student life
+                  
+                  </button>
+                </div>
+              </div>
           </div>
           <div className="col-6">
-          <Image className="img1" src={img1} alt="Bugema University" width="600" height="150"/>
-          {/* <Image className="img2" src={img2} alt="Bugema University" /> */}
-          {/* <div className="hover-image-container">
-              <div className="img-wrapper">
-                <Image className="img1" src={img1} alt="Bugema University" width="200" height="100"/>
-                <Image className="img2" src={img2} alt="Bugema University" />
-              </div>
-              
-            </div> */}
+          <Image
+            className="img1"
+            src={
+              hoveredButton === 'studentLife'
+                ? img
+                : hoveredButton === 'whyBugema'
+                ? img2
+                : img1
+            }
+            alt="Bugema University"
+            width="600"
+            height="150"
+          />
+          {/* <Image className="img1" src={img1} alt="Bugema University" width="600" height="150"/> */}
           </div>
         </div>
 
