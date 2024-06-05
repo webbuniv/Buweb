9f1508b0a7f825529abcb0adb9df4d501feee15b
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2024-06-05 08:42:16 -0700

    below the slider

--- website/components/Common/SectionTitle.tsx
+++ website/components/Common/SectionTitle.tsx
@@ -1,20 +1,20 @@
 const SectionTitle = ({
   title,
   paragraph,
-  width = "570px",
-  center,
+  width = "700px",
+  // center,
   mb = "100px",
 }: {
   title: string;
   paragraph: string;
   width?: string;
-  center?: boolean;
+  // center?: boolean;
   mb?: string;
 }) => {
   return (
     <>
       <div
-        className={`wow fadeInUp w-full ${center ? "mx-auto text-center" : ""}`}
+        className={`wow fadeInUp w-full ${"mx-auto text-center" }`}
         data-wow-delay=".1s"
         style={{ maxWidth: width, marginBottom: mb }}
       >
--- website/components/Features/index.tsx
+++ website/components/Features/index.tsx
@@ -1,6 +1,9 @@
 import SectionTitle from "../Common/SectionTitle";
 import SingleFeature from "./SingleFeature";
+import Image from 'next/image';
 import featuresData from "./featuresData";
+import img1 from "../../public/images/features/bu.jpg";
+import img2 from "../../public/images/features/bu2.jpg";
 
 const Features = () => {
   return (
@@ -10,11 +13,26 @@ const Features = () => {
         className="bg-primary/[.03] py-16 md:py-20 lg:py-28"
       >
         <div className="container">
-          <SectionTitle
-            title="Main Features"
-            paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
-            center
-          />
+        <div className="grid grid-cols-2 gap-x-8 gap-y-14 md:grid-cols-1 lg:grid-cols-2">
+          <div className="col-6">
+            <SectionTitle
+              title="For ambition that can take you anywhere, get your start at Bugema University"
+              paragraph=""
+              // center
+            />
+          </div>
+          <div className="col-6">
+          <Image className="img1" src={img1} alt="Bugema University" width="600" height="150"/>
+          {/* <Image className="img2" src={img2} alt="Bugema University" /> */}
+          {/* <div className="hover-image-container">
+              <div className="img-wrapper">
+                <Image className="img1" src={img1} alt="Bugema University" width="200" height="100"/>
+                <Image className="img2" src={img2} alt="Bugema University" />
+              </div>
+              
+            </div> */}
+          </div>
+        </div>
 
           <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-3">
             {featuresData.map((feature) => (
