1e8f29342fb7fb0cc69169dd8ea1681b14ae5493
TroyMoses <mugabimoses07@gmail.com>
2024-06-08 17:33:33 +0800

    news carousel done

--- website/components/NewsCarousel/NewsCarousel.tsx
+++ website/components/NewsCarousel/NewsCarousel.tsx
@@ -1,136 +1,138 @@
-"use client"
+"use client";
 
-import React from 'react';
-import Link from 'next/link';
-import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
+import React, { useState } from "react";
+import Link from "next/link";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/free-mode';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/free-mode";
 
-import { FreeMode, Pagination, Navigation } from 'swiper/modules';
+import { FreeMode, Pagination, Navigation } from "swiper/modules";
 
-import { RxArrowLeft, RxArrowRight, RxArrowTopRight } from 'react-icons/rx';
-import { ServiceData } from '@/app/constants/news';
-import SectionTitle from '../Common/SectionTitle';
+import { RxArrowLeft, RxArrowRight, RxArrowTopRight } from "react-icons/rx";
+import { ServiceData } from "@/app/constants/news";
+import SectionTitle from "../Common/SectionTitle";
+
+import { motion } from "framer-motion";
+import { staggerContainer, fadeIn } from "@/utils/motion";
 
 export default function NewsCarousel() {
-
   const swiper = useSwiper();
 
   return (
-    <div className=''>
+    <div className="">
+      {/* Latest News Section title */}
+      <SectionTitle
+        title="Our Latest News"
+        paragraph="Stay up to date with the latest trending news in Bugema University. Check out some of the latest news happening around the various campuses."
+      />
 
-        {/* Latest News Section title */}
-        <SectionTitle
-            title="Our Latest News"
-            paragraph="Stay up to date with the latest trending news in Bugema University. Check out some of the latest news happening around the various campuses."      
-        />
+      {/* Carousel on small devices */}
+      <div className="-mt-44 flex h-screen flex-col items-center justify-center md:hidden">
+        <Swiper
+          slidesPerView={1}
+          freeMode={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[FreeMode, Pagination]}
+          className="max-w-[90%] lg:max-w-[80%]"
+        >
+          {ServiceData.map((item, index) => (
+            <SwiperSlide key={item.title} className="">
+              <motion.div
+                variants={fadeIn("right", "spring", index * 0.5, 0.75)} 
+                initial="hidden"
+                whileInView="show"
+                exit="hidden"
+                >
+                <Link href={item.cardlink} className="group relative mx-auto mb-12 flex h-[250px] w-[280px] cursor-pointer flex-col gap-6 overflow-hidden rounded-xl px-6 py-8 text-white shadow-lg lg:h-[400px] lg:w-[350px]">
+                    <div
+                    className="absolute inset-0 bg-cover bg-center transition-all duration-700 group-hover:scale-125"
+                    style={{ backgroundImage: `url(${item.backgroundImage})` }}
+                    />
+                    <div className="absolute inset-0 bg-black opacity-40 group-hover:opacity-50 dark:opacity-50 dark:group-hover:opacity-70" />
+                    <div className="relative flex flex-col gap-3">
+                        <item.icon className="text-blue-600 group-hover:text-blue-400 h-[32px] w-[32px]" />
+                        <h1 className="text-2xl">{item.title}</h1>
+                        <p className="lg:text-[24px]">{item.content}</p>
+                    </div>
+                </Link>
+              </motion.div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
 
-        {/* Carousel on small devices */}
-        <div className='md:hidden -mt-44 flex items-center justify-center flex-col h-screen'>
-            <Swiper 
-                slidesPerView={1}
+      {/* Carousel on big devices */}
+      <div
+        className="-mt-10 hidden h-screen flex-col items-center justify-center md:flex"
+      >
+        <Swiper
+          breakpoints={{
+            340: {
+              slidesPerView: 2,
+              spaceBetween: 15,
+            },
+            700: {
+              slidesPerView: 3,
+              spaceBetween: 15,
+            },
+          }}
+          freeMode={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[FreeMode, Pagination]}
+          className="max-w-[90%] lg:max-w-[80%]"
+        >
+          {ServiceData.map((item, index) => (
+            <SwiperSlide key={item.title} className="">
+              <motion.div
+                variants={fadeIn("right", "spring", index * 0.5, 0.75)} 
+                initial="hidden"
+                whileInView="show"
+                exit="hidden"
+              >
+                <Link
+                  href={item.cardlink}
+                  className="group relative mb-14 flex h-[250px] w-[215px] cursor-pointer flex-col gap-6 overflow-hidden rounded-xl px-6 py-8 text-white shadow-lg lg:h-[400px] lg:w-[350px]"
+                >
+                  <div
+                    className="absolute inset-0 bg-cover bg-center transition-all duration-700 group-hover:scale-125"
+                    style={{ backgroundImage: `url(${item.backgroundImage})` }}
+                  />
+                  <div className="absolute inset-0 bg-black opacity-40 group-hover:opacity-50 dark:opacity-50 dark:group-hover:opacity-70" />
+                  <div className="relative flex flex-col gap-3">
+                    <item.icon className="text-blue-600 group-hover:text-blue-400 h-[32px] w-[32px]" />
+                    <h1 className="text-2xl lg:text-3xl">{item.title}</h1>
+                    <p className="lg:text-[24px]">{item.content}</p>
+                  </div>
+                  <RxArrowTopRight className="group-hover:text-blue-500 absolute bottom-5 left-5 h-[35px] w-[35px] text-white duration-100 group-hover:rotate-45" />
+                </Link>
+              </motion.div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
 
-                freeMode={true}
-                pagination={{
-                    clickable: true
-                }}
-                modules={[FreeMode, Pagination]}
-                className='max-w-[90%] lg:max-w-[80%]'
-            >
-
-                {ServiceData.map((item) => (
-                    <SwiperSlide key={item.title} className=''>
-                        <div className='flex flex-col gap-6 mx-auto mb-12 group relative shadow-lg text-white rounded-xl px-6 py-8 h-[250px] w-[280px] lg:h-[400px] lg:w-[350px] overflow-hidden cursor-pointer'>
-                            <div 
-                                className='absolute inset-0 bg-cover bg-center group-hover:scale-125 transition-all duration-700' 
-                                style={{backgroundImage: `url(${item.backgroundImage})`}}
-                            />
-                            <div 
-                                className='absolute inset-0 bg-black opacity-40 dark:opacity-50 dark:group-hover:opacity-70 group-hover:opacity-50' 
-                            />
-                            <div className="relative flex flex-col gap-3">
-                                <item.icon className='text-blue-600 group-hover:text-blue-400 w-[32px] h-[32px]' />
-                                <h1 className="text-2xl lg:text-3xl">{item.title}</h1>
-                                <p className='lg:text-[24px]'>{item.content}</p>
-                            </div>
-                            <RxArrowTopRight className='absolute bottom-5 left-5 w-[35px] h-[35px] text-white group-hover:text-blue-500 group-hover:rotate-45 duration-100'/>
-                        </div>
-                    </SwiperSlide>
-                ))}
-
-            </Swiper>
-            
-        </div>
-
-        {/* Carousel on big devices */}
-        <div className='hidden md:flex -mt-10 items-center justify-center flex-col h-screen'>
-           
-            <Swiper 
-                breakpoints={{
-                    340: {
-                        slidesPerView: 2,
-                        spaceBetween: 15
-                    },
-                    700: {
-                        slidesPerView: 3,
-                        spaceBetween: 15
-                    }
-                }}
-
-                freeMode={true}
-                pagination={{
-                    clickable: true
-                }} 
-                modules={[FreeMode, Pagination]}
-                className='max-w-[90%] lg:max-w-[80%]'
-            >
-
-                {ServiceData.map((item) => (
-                    
-                    <SwiperSlide key={item.title} className=''>
-                        <div>
-                            <Link href={item.cardlink} className='flex flex-col gap-6 mb-14 group relative shadow-lg text-white rounded-xl px-6 py-8 h-[250px] w-[215px] lg:h-[400px] lg:w-[350px] overflow-hidden cursor-pointer'>
-                                <div 
-                                    className='absolute inset-0 bg-cover bg-center group-hover:scale-125 transition-all duration-700' 
-                                    style={{backgroundImage: `url(${item.backgroundImage})`}}
-                                />
-                                <div 
-                                    className='absolute inset-0 bg-black opacity-40 dark:opacity-50 dark:group-hover:opacity-70 group-hover:opacity-50' 
-                                />
-                                <div className="relative flex flex-col gap-3">
-                                    <item.icon className='text-blue-600 group-hover:text-blue-400 w-[32px] h-[32px]' />
-                                    <h1 className="text-2xl lg:text-3xl">{item.title}</h1>
-                                    <p className='lg:text-[24px]'>{item.content}</p>
-                                </div>
-                                <RxArrowTopRight className='absolute bottom-5 left-5 w-[35px] h-[35px] text-white group-hover:text-blue-500 group-hover:rotate-45 duration-100'/>
-                            </Link>
-                        </div>
-                        
-                    </SwiperSlide>
-                    
-                ))}
-            
-            
-            </Swiper>
-            
-        </div>
-        
-        {/* Swiper nav controllers */}
-        <div className='flex justify-center mx-auto -mt-44 md:-mt-16 mb-10 space-x-4'>
-            <button onClick={() => swiper.slideNext()} className='dark:bg-primary/50 bg-body-color rounded-full p-[1rem] hover:scale-110 transition-all duration-300'>
-                <RxArrowLeft className='font-bold text-white' />
-            </button>
-            <button onClick={() => swiper.slidePrev()} className='dark:bg-primary/50 bg-body-color rounded-full p-[1rem] hover:scale-110 transition-all duration-300'>
-                <RxArrowRight className='font-bold text-white' />
-            </button>
-        </div>
-
+      {/* Swiper nav controllers */}
+      <div className="mx-auto -mt-44 mb-10 flex justify-center space-x-4 md:-mt-16">
+        <button
+          onClick={() => swiper.slideNext()}
+          className="rounded-full bg-body-color p-[1rem] transition-all duration-300 hover:scale-110 dark:bg-primary/50"
+        >
+          <RxArrowLeft className="font-bold text-white" />
+        </button>
+        <button
+          onClick={() => swiper.slidePrev()}
+          className="rounded-full bg-body-color p-[1rem] transition-all duration-300 hover:scale-110 dark:bg-primary/50"
+        >
+          <RxArrowRight className="font-bold text-white" />
+        </button>
+      </div>
     </div>
-
-    
-    
-  )
+  );
 }
