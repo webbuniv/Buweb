b86380a120c2e748145543d02a812bb8e67078ef
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2024-06-10 08:22:06 -0700

    more

--- website/components/Features/index.tsx
+++ website/components/Features/index.tsx
@@ -117,7 +117,7 @@ const Features = () => {
                <div className="grid gap-x-5 gap-y-8 md:grid-cols-1">
                 <div>
                   <div
-                    className="button2 animated2-button rounded-md flex py-1 px-3 md:py-2 md:px-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
+                    className="button2 flex animated2-button rounded-md py-1 px-3 items-center py-2 text-lg md:text-xl lg:text-2xl font-semibold text-white transition bg-blue-500 hover:bg-blue-600"
                     style={{ backgroundColor: hoveredButton === 'under' ? 'blue' : '' }}
                     onMouseEnter={() => setHoveredButton('under')}
                     onMouseLeave={() => setHoveredButton(null)}
@@ -176,15 +176,15 @@ const Features = () => {
             />
           {/* <Image className="img1" src={img1} alt="Bugema University" width="600" height="150"/> */}
           </div>
-          <div className="triangle-top-right" ></div>
-          <div className="triangle-bottom-left" ></div>
+             <div className="triangle-top-right absolute top-0 right-0 border-solid border-transparent border-t-0 border-r-80 border-b-330 border-l-0 border-gray-900"></div>
+             <div className="triangle-bottom-left absolute bottom-0 left-0 border-solid border-transparent border-t-345 border-r-0 border-b-10 border-l-90 border-gray-900"></div>
           </div>
         </div>
         </div>
         <div className="container1 -mt-60 grid grid-cols-3 gap-x-8 md:grid-cols-1 lg:grid-cols-2">
-        <div className="skewed left" ></div>
-        <div className="skewed center"></div>
-        <div className="skewed right"></div>
+            <div className="skewed w-150 h-65 bg-gray-900"></div>
+            <div className="skewed w-150 h-65 bg-gray-900"></div>
+            <div className="skewed w-150 h-65 bg-gray-900"></div>
         </div>
 
           {/* <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-3">
