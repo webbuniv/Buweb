b6d457cafd232372f66d6e8644698b79d5ffa479
TroyMoses <mugabimoses07@gmail.com>
2024-06-13 16:01:50 +0800

    config, schools

--- website/app/constants/news/index.tsx
+++ website/app/constants/news/index.tsx
@@ -5,7 +5,10 @@ import {
     RxReader,
     RxRocket,
     RxAccessibility,
+    
 } from 'react-icons/rx';
+import { BiCross, BiHealth, BiSolidBusiness, BiSolidFactory } from'react-icons/bi';
+import { FaGraduationCap } from 'react-icons/fa';
 
 import Buadmin from '../../../public/images/carousel/buadmin.jpg';
 import Bugate from '../../../public/images/carousel/bugate.jpg';
@@ -13,41 +16,55 @@ import Bb from '../../../public/images/features/bb.jpg';
 import Bu from '../../../public/images/features/bu.jpg';
 import Bulife from '../../../public/images/features/life.jpg';
 
-export const ServiceData = [
+export const SchoolsData = [
     {
-        icon: RxAccessibility,
-        title: "Bursaries",
-        content:"Bugema University gives away over 100 bursaries for new May - Intake students.",
-        backgroundImage: "/images/features/life.jpg",
-        cardlink: "https://www.bugemauniv.ac.ug/bursaries",
+        icon: RxDesktop,
+        title: "School Of Science And Technology",
+        content:"Prepares professionals that can harness the potentials of tech...",
+        backgroundImage: "/images/features/bb.jpg",
+        cardlink: "/school-of-science-and-technology",
     },
     {
         icon: RxPencil2,
-        title: "Evangelism",
-        content:"Theology department sets off over 50 pastors for an evangelistic campaign in Dodoma.",
-        backgroundImage: "/images/carousel/buadmin.jpg",
-        cardlink: "https://www.bugemauniv.ac.ug/evangelism",
-    },
-    {
-        icon: RxDesktop,
-        title: "Facilities",
-        content:"Partners donate 200 desktop computers to facilitate IT students in the computer labs.",
-        backgroundImage: "/images/features/bb.jpg",
-        cardlink: "https://www.bugemauniv.ac.ug/facilities",
-    },
-    {
-        icon: RxReader,
-        title: "Admissions",
-        content:"Admissions for August Intake are currently ongoing at all campuses of Bugema University.",
+        title: "School of Education",
+        content:"A true teacher is one that is mentally, physically and spiritually sound to impart the same virtues in his/her learners...",
         backgroundImage: "/images/carousel/bugate.jpg",
-        cardlink: "https://www.bugemauniv.ac.ug/admissions",
+        cardlink: "/school-of-education",
     },
     {
-        icon: RxCrop,
-        title: "Baptism",
-        content:"Over 30 students baptised into the Seventh Day Adventist Church after evangelistic campaign.",
+        icon: BiCross,
+        title: "School Of Theology And Relig. Studies",
+        content:"Provides spiritual, academic, physical and social development in preparing pastors...",
+        backgroundImage: "/images/features/bb.jpg",
+        cardlink: "/school-of-theology-and-religious-studies",
+    },
+    {
+        icon: BiSolidBusiness,
+        title: "School Of Business",
+        content:"As business times evolve, so are the skills needed to run such environments...",
+        backgroundImage: "/images/features/life.jpg",
+        cardlink: "/school-of-business",
+    },
+    {
+        icon: BiSolidFactory,
+        title: "School of Agric. And Applied Sciences",
+        content:"The basis of the economy not only in Uganda but also in most developing economies around the world...",
+        backgroundImage: "/images/carousel/bugate.jpg",
+        cardlink: "/school-of-agricultural-and-applied-sciences",
+    },
+    {
+        icon: BiHealth,
+        title: "School Of Health And Allied Sciences",
+        content:"More than committed to producing world class medical experts...",
+        backgroundImage: "/images/carousel/buadmin.jpg",
+        cardlink: "/school-of-health-and-allied-sciences",
+    },
+    {
+        icon: FaGraduationCap,
+        title: "School Of Graduate Studies",
+        content:"Offers advanced education and research in a novel process...",
         backgroundImage: "/images/features/bu.jpg",
-        cardlink: "https://www.bugemauniv.ac.ug/baptsim",
+        cardlink: "/school-of-graduate-studies",
     },
 ];
 
--- website/next.config.js
+++ website/next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['cdn.hashnode.com'], 
-    },
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'cdn.hashnode.com',
+        },
+      ],
+  },
 }
 
 module.exports = nextConfig
