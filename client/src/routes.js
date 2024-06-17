41f97cfc49553e0eda09945fe0b1641ccfeca37d
Daniel Ntege <danientege785@gmail.com>
2024-06-17 16:30:46 +0300

    commit

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,16 +1,13 @@
 
 import Dashboard from "./layouts/dashboard";
 import Tables from "./layouts/tables";
-import Billing from "./layouts/billing";
-// import VirtualReality from "./layouts/virtual-reality";
-import RTL from "./layouts/rtl";
 import Profile from "./layouts/profile";
 import SignIn from "./layouts/authentication/sign-in";
 import SignUp from "./layouts/authentication/sign-up";
 import Events from "./layouts/events";
 import News from "./layouts/news";
 import Publication from "./layouts/publication";
-// import Slides from "./layouts/slides";
+import Department from "./layouts/department";
 
 // Soft UI Dashboard React icons
 import Shop from "./examples/Icons/Shop";
@@ -19,7 +16,6 @@ import Settings from "./examples/Icons/Settings";
 import Document from "./examples/Icons/Document";
 import SpaceShip from "./examples/Icons/SpaceShip";
 import CustomerSupport from "./examples/Icons/CustomerSupport";
-import CreditCard from "./examples/Icons/CreditCard";
 import Cube from "./examples/Icons/Cube";
 
 const routes = [
@@ -41,15 +37,15 @@ const routes = [
     component: <Tables />,
     noCollapse: true,
   },
-  // {
-  //   type: "collapse",
-  //   name: "E",
-  //   key: "billing",
-  //   route: "/billing",
-  //   icon: <CreditCard size="12px" />,
-  //   component: <Billing />,
-  //   noCollapse: true,
-  // },
+  {
+    type: "collapse",
+    name: "Dapartment",
+    key: "department",
+    route: "/department",
+    icon: <Cube size="12px" />,
+    component: <Department />,
+    noCollapse: true,
+  },
   {
     type: "collapse",
     name: "News",
@@ -90,8 +86,8 @@ const routes = [
   {
     type: "collapse",
     name: "Sign In",
-    key: "sign-in",
-    route: "/authentication/sign-in",
+    key: "signin",
+    route: "/sign",
     icon: <Document size="12px" />,
     component: <SignIn />,
     noCollapse: true,
