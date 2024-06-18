253a9dd5d59eb056dfd57bb9e6bff5517ad00039
Daniel Ntege <danientege785@gmail.com>
2024-06-18 11:10:41 +0300

    commit

--- website/app/head.tsx
+++ website/app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>Bugema University</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Bugema University Website" />
-      <link rel="favicon" href="/images/favicon.ico" />
+      <link rel="favicon" href="/images/favicon.ico"  />
     </>
   );
 }
