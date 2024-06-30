7cf727e5675c4f266e0a8d89d680cd710f06110b
AKATWIJUKA-ELIA <eliaakjtrnq@gmail.com>
2024-06-30 09:54:04 +0300

    upadate now

--- website/components/Header/index.tsx
+++ website/components/Header/index.tsx
@@ -12,7 +12,6 @@ import image from "../../public/images/logo/logo1.png";
 import "../../styles/nav.css";
 import "../../styles/index.css";
 import { FaChevronDown } from "react-icons/fa";
-import MobileNav from "../MobileNav/MobileNav";
 
 const Header = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
@@ -24,13 +23,6 @@ const Header = () => {
   const [sticky, setSticky] = useState(false);
   const [openIndex, setOpenIndex] = useState(-1);
   const [showModel2, setShowModel2] = useState(false);
-
-  const [programsOpen, setProgramsOpen] = useState(false);
-  const [studentLifeOpen, setStudentLifeOpen] = useState(false);
-  const [researchOpen, setResearchOpen] = useState(false);
-  const [campusesOpen, setCampusesOpen] = useState(false);
-
-
   const navbarToggleHandler = () => {
     setNavbarOpen(!navbarOpen);
   };
@@ -57,9 +49,9 @@ const Header = () => {
     }
   };
   useEffect(() => {
-    document.body.style.overflow = showModel ? "hidden" : "auto";
+    document.body.style.overflow = showModel ? 'hidden' : 'auto';    
   }, [showModel]);
-  const first_modal = () => {
+  const first_modal = () =>{
     setShowModel(true);
     setShowModel1(false);
     setShowModel2(false);
@@ -67,27 +59,27 @@ const Header = () => {
     setShowModel4(false);  
      
   };
+  
 
   useEffect(() => {
-    document.body.style.overflow = showModel1 ? "hidden" : "auto";
+    document.body.style.overflow = showModel1 ? 'hidden' : 'auto';
+          
   }, [showModel1]);
-  const second_modal = () => {
-    setShowModel1(true),
-      (document.body.style.overflow = showModel1 ? "hidden" : "auto");
+  const second_modal =() =>{
+    setShowModel1(true), document.body.style.overflow = showModel1 ? 'hidden' : 'auto';
     setShowModel(false);
     setShowModel2(false);
     setShowModel3(false);
     setShowModel4(false);  
      
     
-
   };
 
   useEffect(() => {
-    document.body.style.overflow = showModel2 ? "hidden" : "auto";
+    document.body.style.overflow = showModel2 ? 'hidden' : 'auto';           
   }, [showModel2]);
 
-  const third_modal = () => {
+  const third_modal =() =>{
     setShowModel2(true);
     setShowModel(false);
     setShowModel1(false);
@@ -97,28 +89,28 @@ const Header = () => {
   };  
 
   useEffect(() => {
-    document.body.style.overflow = showModel3 ? "hidden" : "auto";
-  }, [showModel3]);
+    document.body.style.overflow = showModel3 ? 'hidden' : 'auto';         
+  }, [showModel3]); 
 
-  const forth_modal = () => {
+  const forth_modal =() =>{
     setShowModel3(true);
-    setShowModel(false);
+    setShowModel(false); 
     setShowModel1(false);
     setShowModel2(false);
-
     setShowModel4(false);  
                 
   }
   useEffect(() => {
-    document.body.style.overflow = showModel4 ? "hidden" : "auto";
-  }, [showModel4]);
-  const fifth_modal = () => {
+    document.body.style.overflow = showModel4 ? 'hidden' : 'auto';        
+  }, [showModel4]); 
+  const fifth_modal =() =>{
     setShowModel4(true);
     setShowModel3(false);
-    setShowModel(false);
+    setShowModel(false); 
     setShowModel1(false);
     setShowModel2(false);
-  
+    
+    
     
   };
 
@@ -133,52 +125,35 @@ const Header = () => {
         
   return (
     <>
-    {/* Header on big screens */}
-      <header
-        className={`hidden header top-0 left-0 z-40 lg:flex w-full items-center  mt-[-8px] ${
-          sticky
-            ? " bg-dark !fixed !z-[9999] ! bg-opacity-100 shadow-sticky backdrop-blur-sm fade-in !transition dark:! dark:!bg-opacity-100"
-            : "absolute"
-        }`}
-      >
+      <header className={`header top-0 left-0 z-40 flex w-full items-center  mt-[-8px] ${sticky ? " bg-dark !fixed !z-[9999] ! bg-opacity-100 shadow-sticky backdrop-blur-sm fade-in !transition dark:! dark:!bg-opacity-100" : "absolute"}`}>
         <div className="container">
           <div className="relative flex items-center justify-between">
-            <div className="w-60 relative z-10">
-              <Link
-                href="/"
-                className={`header-logo block w-full ${
-                  sticky ? "py-2 lg:py-2" : "py-3"
-                }`}
-              >
-                <Image
-                  src={image}
-                  alt="logo"
-                  width={140}
-                  height={40}
-                  className="w-full dark:hidden"
-                />
-                <Image
-                  src={image}
-                  alt="logo"
-                  width={100}
-                  height={20}
-                  className="hidden w-full dark:block"
-                />
-              </Link>
+            <div className="w-60 relative z-10" >
+                  <Link href="/"  className={`header-logo block w-full ${sticky ? "py-2 lg:py-2" : "py-3"}`}>
+                        <Image src={image} alt="logo" width={140} height={40} className="w-full dark:hidden" />
+                        <Image src={image} alt="logo" width={100} height={20} className="hidden w-full dark:block" />
+                         
+                  </Link>
+                 
             </div>
             <div className="flex space-x-20 justify-between transform bg-white">
               <div>
-                <button
-                  onClick={() => {
-                    setNavbarOpen(!navbarOpen);
-                    setProgramsOpen(false);
-                    setStudentLifeOpen(false);
-                    setResearchOpen(false);
-                    setCampusesOpen(false);
-                  }}
+
+              <button
+                  onClick={navbarToggleHandler}
                   id="navbarToggler"
                   aria-label="Mobile Menu"
-                  className="absolute right-4 top-1/2 block translate-y-[-50%] rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden"
+                  className="absolute right-4 top-1/2 block translate-y-[-50%] rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden">
+                  <span className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${navbarOpen ? " top-[7px] rotate-45" : " "}`} />
+                  <span className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${navbarOpen ? "opacity-0 " : " "}`} />
+                  <span className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${navbarOpen ? " top-[-8px] -rotate-45" : " "}`} />
+                </button>
+                
+                <nav
+                  id="navbarCollapse"
+                  className={`navbar absolute right-0 z-30 w-[250px] rounded border-[.5px] border-body-color/50 bg-primary py-4 px-6 duration-300 ease-out transition-transform transform dark:border-body-color/20 dark:bg-white lg:visible lg:static lg:w-auto lg:border-none lg:bg-dark lg:p-0 lg:opacity-100 ${
+                    navbarOpen ? "top-full opacity-100 right-0" : "top-[120%] opacity-0 right-[-250px]"
+                  }`}
                 >
                   <ul className="block lg:flex bg-white lg:space-x-8 top-0 left-0 h-full  text-black">
                     <li className="group relative">
@@ -215,7 +190,6 @@ const Header = () => {
 
 
 
-
                     <li>
                       <Link
                         href=""
@@ -243,41 +217,7 @@ const Header = () => {
                       </Link>
                     </li>
 
-
-                      <li>
-                        <Link
-                          href=""
-                          className={`nav hover flex py-2 text-lg text-black  font-bold group-hover:opacity-70 lg:mr-5 lg:inline-flex lg:py-6 lg:px-0`}
-                          onMouseEnter={third_modal}
-                          onMouseLeave={close}
-                        >
-                          <span className={showModel2 ? "active" : ""}>
-                            Research
-                          </span>
-                          <span className={"my-1 ml-2 dark:text-dark "}>
-                            <FaChevronDown
-                              className={showModel2 ? "drop" : ""}
-                            />
-                          </span>
-                        </Link>
-                      </li>
-                      <li>
-                        <Link
-                          href=""
-                          className={`nav hover flex py-2 text-lg text-black  font-bold group-hover:opacity-70 lg:mr-5 lg:inline-flex lg:py-6 lg:px-0`}
-                          onMouseEnter={forth_modal}
-                          onMouseLeave={close}
-                        >
-                          <span className={showModel3 ? "active" : ""}>
-                            Campuses
-                          </span>
-                          <span className="my-1 ml-2 dark:text-dark ">
-                            <FaChevronDown
-                              className={showModel3 ? "drop" : ""}
-                            />
-                          </span>
-                        </Link>
-                      </li>
+                    
 
                     <li>
                       <Link
@@ -294,101 +234,32 @@ const Header = () => {
                     </li>
                   </ul>
                 </nav>
-
               </div>
             </div>
           </div>
           <Model isvisible={showModel} onClose={() => setShowModel(false)}>
-            <></>
+            <>
+            </>
           </Model>
           <Model1 issvisible={showModel1} onClose={() => setShowModel1(false)}>
-            <></>
+            <>
+            </>
           </Model1>
           <Model2 isssvisible={showModel2} onClose={() => setShowModel2(false)}>
-            <></>
+            <>
+            </>
           </Model2>
           <Model3 is3visible={showModel3} onClose={() => setShowModel3(false)}>
-            <></>
+            <>
+            </>
           </Model3>
           <Model4 is4visible={showModel4} onClose={() => setShowModel4(false)}>
-            <></>
+            <>
+            </>
           </Model4>
           
         </div>
       </header>
-
-      {/* Header on small screens */}
-      <header
-        className={`lg:hidden header top-0 left-0 z-40 flex w-full items-center bg-dark mt-[-8px] ${
-          sticky
-            ? " bg-dark !fixed !z-[9999] ! bg-opacity-100 shadow-sticky backdrop-blur-sm fade-in !transition dark:! dark:!bg-opacity-100"
-            : "absolute"
-        }`}
-      >
-        <div className="container">
-          <div className="relative flex items-center justify-between">
-            <div className="w-60 relative z-10">
-              <Link
-                href="/"
-                className={`header-logo block w-full ${
-                  sticky ? "py-2 lg:py-2" : "py-3"
-                }`}
-              >
-                <Image
-                  src={image}
-                  alt="logo"
-                  width={140}
-                  height={40}
-                  className="w-full dark:hidden"
-                />
-                <Image
-                  src={image}
-                  alt="logo"
-                  width={100}
-                  height={20}
-                  className="hidden w-full dark:block"
-                />
-              </Link>
-            </div>
-            <div className="flex space-x-20 justify-between transform bg-white">
-              <div>
-                <button
-                  onClick={() => {
-                    setNavbarOpen(!navbarOpen);
-                    setProgramsOpen(false);
-                    setStudentLifeOpen(false);
-                    setResearchOpen(false);
-                    setCampusesOpen(false);
-                  }}
-                  id="navbarToggler"
-                  aria-label="Mobile Menu"
-                  className="absolute right-4 top-1/2 block translate-y-[-50%] rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden"
-                >
-                  <span
-                    className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${
-                      navbarOpen ? " top-[7px] rotate-45" : " "
-                    }`}
-                  />
-                  <span
-                    className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${
-                      navbarOpen ? "opacity-0 " : " "
-                    }`}
-                  />
-                  <span
-                    className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${
-                      navbarOpen ? " top-[-8px] -rotate-45" : " "
-                    }`}
-                  />
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </header>
-
-      {navbarOpen && (
-        <MobileNav setNavbarOpen={setNavbarOpen} navbarOpen={navbarOpen}/>
-      )}
     </>
   );
 };
--- website/components/HeroOverlay/HeroOverlay.tsx
+++ website/components/HeroOverlay/HeroOverlay.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import React from 'react';
 import { FaArrowDown } from 'react-icons/fa6';
 
-const HeroOverlay = ({ title,title2, subtitle,subtitle2, buttonText, buttonLink }) => {
+const HeroOverlay = ({ title,title2,title3, subtitle,subtitle2,subtitle3, buttonText, buttonLink }) => {
   return (
       <>
             <div className="hidden hero-overlay lg:flex flex-col space-y-4 justify-center mx-auto items-center mt-4">
@@ -23,12 +23,17 @@ const HeroOverlay = ({ title,title2, subtitle,subtitle2, buttonText, buttonLink 
 
             <div  className='hero-overlay2 '>
             <div className='flex-1'>
-                  <h1 className='text-start sm:text-6xl text-4xl font-medium font-bold mb-0'>{title2}</h1>
+                  <h1 className='text-start sm:text-6xl text-4xl font-medium font-bold mb-0'>{title3}</h1>
+            </div>
+            <div className='flex-1'>
+                  <h1 className=' text-start text-8xl text-4xl font-medium font-bold mb-0'>{title2}</h1>
             </div>
 
                   <div className='flex-1 hidden sm:block'> {/* Hide on small screens, show on medium and above */}
-                   <h2 className='text-start mt-5'>{subtitle2}</h2>
+                   <h2 className='text-start text-2xl mt-5'>{subtitle2},<br />{subtitle3}</h2>
                   </div>
+
+                 
             </div>
 
             
