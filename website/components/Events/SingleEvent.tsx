abc557409a8427989dc11a19535171513c2e7793
TroyMoses <mugabimoses07@gmail.com>
2024-07-01 03:09:28 +0800

    event

--- website/components/Events/SingleEvent.tsx
+++ website/components/Events/SingleEvent.tsx
@@ -4,22 +4,30 @@ import Link from "next/link";
 import { News } from "@/types/types";
 
 type Props = {
-    post: News;
-}
+  post: News;
+};
 
 const SingleEvent = ({ post }: Props) => {
   return (
     <div className="wow fadeInUp relative overflow-hidden rounded-md bg-white shadow-one h-[450px] dark:bg-dark w-[220] mt-5">
-      <Link href={`/news/${post._id}`} passHref className="relative block h-[220px] w-full">
-          <span className="absolute top-6 right-6 z-20 inline-flex items-center justify-center rounded-full bg-primary py-2 px-4 text-sm font-semibold capitalize text-white">
-            {post.title}
-          </span>
-          <Image src={post.photo} alt="news" layout="fill" />
+      <Link
+        href={`/news/${post._id}`}
+        passHref
+        className="relative block h-[220px] w-full"
+      >
+        <span className="absolute top-6 right-6 z-20 inline-flex items-center justify-center rounded-full bg-primary py-2 px-4 text-sm font-semibold capitalize text-white">
+          {post.title}
+        </span>
+        <Image src={post.photo} alt="news" layout="fill" />
       </Link>
       <div className="p-6 sm:p-8 md:py-8 md:px-6 lg:p-8 xl:py-8 xl:px-5 2xl:p-8">
         <h3>
-          <Link href={`/news/${post._id}`} passHref className="mb-4 block text-xl font-bold text-black hover:text-primary dark:text-white dark:hover:text-primary sm:text-2xl">
-              {post.category}
+          <Link
+            href={`/news/${post._id}`}
+            passHref
+            className="mb-4 block text-xl font-bold text-black hover:text-primary dark:text-white dark:hover:text-primary sm:text-2xl"
+          >
+            {post.category}
           </Link>
         </h3>
         <p className="mb-6 border-b border-body-color border-opacity-10 pb-6 text-base font-medium text-body-color dark:border-white dark:border-opacity-10">
