7d110fe157e005b8d6345269882353302fde7199
Daniel Ntege <danientege785@gmail.com>
2024-07-02 14:16:36 +0300

    commit

--- website/components/About/AboutSectionLife.tsx
+++ website/components/About/AboutSectionLife.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import YouTubeEmbed from '@/components/Youtube';
 import { fetchYouTubeVideos } from '@/lib/youtube';
+import SectionTitle from "../Common/SectionTitle";
 
 const HomePage: React.FC = () => {
   const [videos, setVideos] = useState([]);
@@ -17,7 +18,12 @@ const HomePage: React.FC = () => {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Latest Videos</h1>
+      <SectionTitle
+          title="Our Latest Videos"
+          paragraph="You'll find a wealth of 
+        knowledge and insights on various topics related to academia, student life, research, and more."
+          center
+        />
       <div className="flex flex-wrap -mx-4">
         {videos.map((video) => (
           <YouTubeEmbed
