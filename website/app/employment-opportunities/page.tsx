117143ff4a6c1213d9ecd8cd01ac689aa2a7c3e2
TroyMoses <mugabimoses07@gmail.com>
2024-07-04 18:50:11 +0800

    jobs

---
+++ website/app/employment-opportunities/page.tsx
@@ -0,0 +1,117 @@
+"use client";
+
+import React from "react";
+
+export default function EmploymentOpportunities() {
+  return (
+    <>
+      <section className="pt-[120px] pb-[120px]">
+
+        <div className="container my-2">
+
+          <div className="py-2 text-start lg:text-center border-b border-body-color border-opacity-10 pb-10">
+            <h1 className="text-2xl text-green-500">Employment Opportunitiesites</h1>
+            <h2 className="text-xl text-blue-500">External Advert June 2024</h2>
+          </div>
+
+          <div className=" text-body-color border-b border-body-color border-opacity-10 pb-10">
+            <p>
+              Applications are invited from suitably qualified candidates for
+              the following vacant positions at Bugema University. Please see
+              the job details on Bugema University Website:{" "}
+              <span>www.bugemauniv.ac.ug</span>
+              and submit your application through{" "}
+              <span>bugemahumanresource@gmail.com</span>
+              Not Later than on the 15, July, 2024 at 5pm. Hardcopies shall not
+              be accepted.
+            </p>
+            <p>
+              All applicants must attach relevant soft copies of academic
+              transcripts, certificates and relevant appointment letters.{" "}
+              <span>PDF copies maximum 10MB.</span>
+            </p>
+            <p>
+              Only shortlisted candidates shall be contacted for interviews. All
+              documents tendered in shall be verified with the relevant
+              authorities. Bugema University provides equal opportunities to
+              everyone. Applicants who shall not hear from Bugema University on
+              completion of the selection exercise should consider themselves as
+              unsuccessful.
+            </p>
+            <p>
+              Bugema University is located 32 Kilometers north of Kampala along
+              Gayaza – Zirobwe Road. Telephone Mainline:{" "}
+              <span>+256 312 351 400</span>, HRM <span>+256 312 351 410</span>
+            </p>
+          </div>
+        </div>
+
+        <div className=" text-body-color py-2">
+          <div className="flex flex-col gap-4">
+            <h2 className="font-semibold">
+              Job Title: <span className=" font-normal">REGISTRAR OF ADMISSIONS AND RECORDS</span>
+            </h2>
+            <h2 className="font-semibold">
+              Vacancies: <span className=" font-normal">01</span>
+            </h2>
+            <h2 className="font-semibold">
+              Responsilbe To: <span className=" font-normal">The DVC-Academics</span>
+            </h2>
+            <h2 className="font-semibold">
+              Terms of Employment:{" "}
+              <span className=" font-normal">
+                Five years performance contract renewable once upon satisfactory
+                performance.
+              </span>
+            </h2>
+          </div>
+
+          <div className="py-4">
+            <div className="py-2">
+              <h1>Purpose of the Job</h1>
+
+              <ul>
+                <li>
+                  <span>
+                    An officer responsible for the Admission, registration,
+                    enrollment, and academic grades of students.
+                  </span>
+                </li>
+              </ul>
+            </div>
+
+            <div className="py-2">
+              <h1>Minimum Qualifications</h1>
+
+              <ul>
+                <li>
+                  <span>Mature, vibrant person aged between 35-55</span>
+                </li>
+                <li>
+                  <span>
+                    Masters Degree in Education Management or curriculum
+                    development, and computer knowledge is a must. A post
+                    graduate qualification in education management, public
+                    administration or a related field if the Masters field is
+                    from other fields other than Education.
+                  </span>
+                </li>
+                <li>
+                  <span>
+                    At least 5 years of actual academic administrative
+                    experience in students records in an academic or institution
+                    of higher learning. Or Having served in the position of
+                    Deputy registrar for three years or Senior Assistant
+                    registrar for five years in a recognised Higher Education
+                    Institution or University. Subscription to Bugema University
+                    norms and core values is a must
+                  </span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
