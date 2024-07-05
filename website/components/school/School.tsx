941225c41ed367fe4760fe7de0b40146209eb21d
AKATWIJUKA-ELIA <eliaakjtrnq@gmail.com>
2024-07-05 15:49:10 +0300

    schl updat

--- website/components/school/School.tsx
+++ website/components/school/School.tsx
@@ -1,9 +1,8 @@
-"use client";
 import Image from "next/image";
 import nurses1 from "../../public/images/nav/nurses1.jpg";
 import image from "../../public/images/nav/labs.jpg";
 import scie from "../../public/images/scienceTech/scie.png";
-import { useEffect, useState } from "react";
+import { Carousel } from "flowbite-react";
 
 const School = ({
   tittle, 
@@ -28,80 +27,48 @@ const School = ({
   center?: boolean;
   mb?: string;
 }) => {
-
-      const [firstslide, SetFirstslide] = useState(false);
-      const [secondslide, SetSecondslide] = useState(false);
-      const [thirdslide, SetThirdslide] = useState(false);
-      const [forthslide, SetForthslide] = useState(false);
-
   return (
       
-      <div className="">
+      <div className="w-screen -ml-12 -my-14">
 
-      
-            
-
-            <div id="default-carousel" className="relative w-full" data-carousel="slide">
-   
-                        <div className="relative h-56 overflow-hidden rounded-lg md:h-full">
-                              {/* Item 1  */}
-                              <div  className="hidden md:block duration-700 ease-in-out" data-carousel-item>
-                                    <div className="overflow-hidden relative -my-10 bg-black w-full left-0" >
-                                          <div className="absolute justify-center ml-[2%] my-[24%]"> 
-                                                <h1 className="font-bold text-start text-xs text-white md:text-7xl">
-                                                      {tittle} 
-                                                </h1>
-                                                <h1 className=" hidden my-5 text-start  text-white md:text-3xl md:block ">{subtittle}</h1>
-                                          </div>
+            <Carousel slideInterval={4000}>
+                  <div className="overflow-hidden rounded-lg relative -my-10 bg-black w-full " >
+                        <div className="absolute justify-center ml-[2%] my-[24%]"> 
+                              <h1 className="font-bold text-start text-xs text-white md:text-7xl">
+                                    {tittle} 
+                              </h1>
+                              <h1 className=" hidden my-5 text-start  text-white md:text-3xl md:block ">{subtittle}</h1>
+                              </div>
                               
-                                          <Image src={topImg} alt='img' objectFit="contain" width={1920} height={500} className="opacity-50 "/>
-      
-                                    </div>
-
-                              </div> 
-                              {/* Item 2  */}
-                              <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                              <Image src={image} alt='img' objectFit="contain" width={1920} height={500} className="opacity-50 "/>
-                              </div>
-                              {/* Item 3  */}
-                              <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                                    
-                              </div>
-                              {/* Item 4  */}
-                              <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                                    
-                              </div>
-                              {/* Item 5  */}
-                              <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                                    
-                              </div>
-                        </div>
-                  
-                        <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-                              <button type="button" className="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-                              <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-                              <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-                              <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
-                              <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
-                        </div>
-                  
-                        <button type="button" className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-                              <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                                    <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
-                                    </svg>
-                                    <span className="sr-only">Previous</span>
-                              </span>
-                        </button>
-                        <button type="button" className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-                              <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                                    <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-                                    </svg>
-                                    <span className="sr-only">Next</span>
-                              </span>
-                        </button>
+                        <Image src={topImg} alt='img' objectFit="contain" width={1920} height={500} className="opacity-50 "/>
+                        
                   </div>
+
+                  <div className="overflow-hidden rounded-lg relative -my-10 bg-black w-full " >
+                        <div className="absolute justify-center ml-[2%] my-[24%]"> 
+                              <h1 className="font-bold text-start text-xs text-white md:text-7xl">
+                                    {tittle} 
+                              </h1>
+                              <h1 className=" hidden my-5 text-start  text-white md:text-3xl md:block ">{subtittle}</h1>
+                              </div>
+                              
+                        <Image src={scie} alt='img' objectFit="contain" width={1920} height={500} className="opacity-50 "/>
+                        
+                  </div>
+
+                  <div className="overflow-hidden rounded-lg relative -my-10 bg-black w-full " >
+                        <div className="absolute justify-center ml-[2%] my-[24%]"> 
+                              <h1 className="font-bold text-start text-xs text-white md:text-7xl">
+                                    {tittle} 
+                              </h1>
+                              <h1 className=" hidden my-5 text-start  text-white md:text-3xl md:block ">{subtittle}</h1>
+                              </div>
+                              
+                        <Image src={nurses1} alt='img' objectFit="contain" width={1920} height={500} className="opacity-50 "/>
+                        
+                  </div>
+            </Carousel>
+            
 
             <div className=" overflow-hidden md:overflow-auto flex flex-col space-y-12">
                   
