013cdb23bbf0528c365870b0d6c6c076b6acd106
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2024-07-07 01:03:59 -0700

    more about courses

--- website/app/courses/school-of-agric/page.tsx
+++ website/app/courses/school-of-agric/page.tsx
@@ -1,21 +1,27 @@
 import SectionTitle from "@/components/Common/SectionTitle";
-import School from "@/components/school/School";
+import Course from "@/components/course/Course";
 import React from "react";
 
 export default function SchoolOfAgric() {
   return (
     <div>
       <section className="my-20 mt-36 mx-10">
-        
+        <Course
+         tittle="Courses in the School of Agriculture and Applied Sciences"
+         subtittle="This school is designed to train and produce well-groomed scientists and technicians who are capable of working under varying and changing local and global environments in both developed and developing economies."
+         topImg={"/images/schools/agric.jpg"}
+          
+        />
       </section>
 
       <div className="flex flex-col items-start mx-auto md:pl-28">
-        <div className="md:pl-2">
+        {/* <div className="md:pl-2">
           <SectionTitle title="Courses in the school of agriculture" paragraph="" />
-        </div>
-        <h3 className="mb-5 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
-        Department Of Agricultural Sciences
-      </h3>
+        </div> */}
+       <h3 className="mb-5 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl" style={{ color: 'blue' }}>
+          Department Of Agricultural Sciences
+        </h3>
+
         <div className='md:pl-2 mx-auto md:mx-0'>
           <ul className='flex flex-col space-y-5 text-body-color'>
             <li>Bachelor of science in Agriculture with options</li>
@@ -26,7 +32,7 @@ export default function SchoolOfAgric() {
           </ul>
         </div>
 
-        <h3 className="mb-5 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
+        <h3 className="mb-5 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl" style={{ color: 'blue' }}>
         Department Of Environmental And Applied Sciences
       </h3>
         <div className='md:pl-2 mx-auto md:mx-0'>
@@ -37,6 +43,8 @@ export default function SchoolOfAgric() {
           </ul>
         </div>
       </div>
+     );
+
     </div>
   );
 }
--- website/app/courses/school-of-business/page.tsx
+++ website/app/courses/school-of-business/page.tsx
@@ -1,20 +1,24 @@
 import SectionTitle from "@/components/Common/SectionTitle";
-import School from "@/components/school/School";
+import Course from "@/components/course/Course";
 import React from "react";
 
 export default function SchoolOfBusiness() {
   return (
     <div>
       <section className="my-20 mt-36 mx-10">
-       
+        <Course
+          tittle="Courses in the School of Business"
+          subtittle="The School of Business is a leading business school in the country. It is a center"
+          topImg=""
+          />
       </section>
 
       <div className="flex flex-col items-start mx-auto md:pl-28">
-        <div className="md:pl-2">
+        {/* <div className="md:pl-2">
           <SectionTitle title="Courses in school of business" paragraph="" />
-        </div>
+        </div> */}
 
-        <h3 className="mb-5 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
+        <h3 className="mb-5 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl" style={{ color: 'blue' }}>
         Department Of Accounting And Finance
       </h3>
 
@@ -28,7 +32,7 @@ export default function SchoolOfBusiness() {
           </ul>
         </div>
 
-        <h3 className="mb-5 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
+        <h3 className="mb-5 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl" style={{ color: 'blue' }}>
         Department Of Management
       </h3>
         <div className='md:pl-2 mx-auto md:mx-0'>
