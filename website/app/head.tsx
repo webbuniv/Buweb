70ff81f5ad61850ce04651d8de7b92e5f6c39aa1
Daniel Ntege <danientege785@gmail.com>
2024-07-08 11:27:50 +0300

    commit

--- website/app/head.tsx
+++ website/app/head.tsx
@@ -1,3 +1,5 @@
+import Script from 'next/script';
+
 export default function Head() {
   return (
     <>
@@ -5,6 +7,18 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Bugema University Website" />
       <link rel="favicon" href="/images/favicon.ico"  />
+      <Script
+          id="google-analytics"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-TE9HEEZ2BK');
+            `,
+          }}
+        />
     </>
   );
 }
