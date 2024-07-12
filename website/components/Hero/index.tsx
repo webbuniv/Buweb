16afbd18e330f2e8b87dc048b9ee8d34594112d3
TroyMoses <mugabimoses07@gmail.com>
2024-07-12 21:22:50 +0800

    more banners

--- website/components/Hero/index.tsx
+++ website/components/Hero/index.tsx
@@ -11,7 +11,9 @@ import "slick-carousel/slick/slick-theme.css";
 
 export const heroMedia = [
   { type: 'video', src: "https://res.cloudinary.com/duaqiajka/video/upload/v1719686933/WELCOME_VIDEO_og2jma.mp4" },
-  { type: 'image', src: "/images/vc/chancellor_banner2.jpg" }
+  { type: 'image', src: "/images/vc/chancellor_banner2.jpg" },
+  { type: 'image', src: "/images/banners/it.jpg" },
+  { type: 'image', src: "/images/banners/nursing.jpg" }
 ];
 
 const textVariants = {
