cee05adbc9a98413b524fe1b4448c271fb3951a1
TroyMoses <mugabimoses07@gmail.com>
2024-07-12 18:39:49 +0800

    hero on large

--- website/components/BelowHero/index.tsx
+++ website/components/BelowHero/index.tsx
@@ -29,7 +29,7 @@ const AboutSectionLife2 = () => {
           }, []);
 
   return (
-    <section className="top-0">
+    <section className="top-0 lg:mt-[500px]">
             <div className="container  -my-24">
                   <div className="container mx-auto">
                         <div className="flex flex-wrap -mx-4 gap-2 lg:gap-0">
