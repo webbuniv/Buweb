cfe01ce9448293ea191e15714364d425f978fa42
Daniel Ntege <danientege785@gmail.com>
2024-09-30 21:19:46 +0300

    commit

--- website/components/MatureEntryAd/MatureEntryAd.tsx
+++ website/components/MatureEntryAd/MatureEntryAd.tsx
@@ -1,10 +1,8 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import MaturePopup from "../MaturePopup/MaturePopup";
 
 export default function MatureEntryAd() {
   const [isVisible, setIsVisible] = useState(false);
-  const [isPopupVisible, setIsPopupVisible] = useState(false);
 
   useEffect(() => {
     // Button is displayed after scrolling for 500 pixels
@@ -23,15 +21,16 @@ export default function MatureEntryAd() {
 
   return (
     <div className="fixed bottom-8 left-8 z-[99]">
-
-        <div
-          onClick={() => setIsPopupVisible(!isPopupVisible)}
-          aria-label="scroll to top"
-          className="flex px-1 h-10 w-[240px] cursor-pointer items-center justify-center rounded-md bg-primary text-black shadow-md transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp"
-        >
-          <span className="text-white">Calender</span>
-
-        </div>
+      {isVisible && (
+        <Link href="/calendar">
+          <div
+            aria-label="go to calendar"
+            className="flex px-1 h-10 w-[240px] cursor-pointer items-center justify-center rounded-md bg-primary text-black shadow-md transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp"
+          >
+            <span className="text-white">Calendar</span>
+          </div>
+        </Link>
+      )}
     </div>
   );
 }
