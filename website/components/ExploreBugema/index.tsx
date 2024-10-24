68b1160d8896e1a31b12f31de4f8891522b3d2b7
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2024-10-24 04:54:24 -0700

    vidz

--- website/components/ExploreBugema/index.tsx
+++ website/components/ExploreBugema/index.tsx
@@ -105,11 +105,15 @@ const Video = () => {
         className={`wow fadeInUp overflow-hidden rounded-md h-[400px]  ${animate ?"slider slide--slower":"" }`} 
         data-wow-delay=".15s">
            
-            <iframe width="500" height="300" title="YouTube video player"  allow="accelerometer; loop=1; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen
+            {/* <iframe width="500" height="300" title="YouTube video player"  allow="accelerometer; loop=1; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen
                 src="https://www.youtube.com/embed/MNurEKZ0T20?si=7pRV-TCvIL1v9dPz"
                 className='w-full h-full'>
-            </iframe>
+            </iframe> */}
 
+            <iframe width="500" height="300" title="YouTube video player"  allow="accelerometer; loop=1; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen
+                src="https://www.youtube.com/embed/8lEWpGiwZVA"
+                className='w-full h-full'>
+            </iframe>
         
       </div>
 
