56ab47459722191b63962a4f3ffdae1e0ed8dd49
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2024-10-26 10:18:04 -0700

    Slidere

--- website/components/AboveHeader/index.tsx
+++ website/components/AboveHeader/index.tsx
@@ -34,8 +34,9 @@ const AboveHeader = () => {
 
   useEffect(() => {
     const statements = [
-      "Welcome To Bugema University",
-      "Discover the Power of Knowledge",
+      "Greetings from Bugema University",
+      "Step Into Excellence at Bugema University!",
+      "Unleash the Power of Knowledge",
       "Stay Tuned for Exciting Upcoming Events!",
       "Celebrate Success!",
       "Graduation from 8th to 19th Nov 2024"
