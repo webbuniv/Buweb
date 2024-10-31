a192e5a8a2aaf0130bb6b5bdb3489c739699e27d
AKATWIJUKA-ELIA <eliaakjtrnq@gmail.com>
2024-10-31 17:03:39 +0300

    complex

---
+++ website/app/complex/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+
+export 
+
+const Complex = () => {
+  return (
+    <div className='mx-auto '>
+
+     <div className='ml-45 py-40'>
+      <h1 className='text-center font-bold md:text-3xl '>PROPOSED COMPLETION OF LABORATORY BUILDING AT BUGEMA UNIVERSITY,LUWERO</h1>
+      
+      <h1 className='text-center text-3xl rounded-md' >
+      <Link href="/docs/complex.JPEG"target="_blank" rel="noopener noreferrer" download className='bg-blue-500 border'>download</Link>
+      </h1>
+      
+
+            
+     </div>
+
+    </div>
+  )
+}
+
+export default Complex;
