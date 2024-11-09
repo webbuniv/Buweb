08928f39077c6d0f73c21c75822c5e1c1a380f6d
AKATWIJUKA-ELIA <eliaakjtrnq@gmail.com>
2024-11-09 15:41:17 +0300

    update

--- website/components/ExploreGraduation/index.tsx
+++ website/components/ExploreGraduation/index.tsx
@@ -96,7 +96,7 @@ const ExploreGraduation = () => {
                 src="https://www.youtube.com/embed/6HUQdC6WwbA?si=NFVTl_-aO3p2MsPI"
                 className="w-full h-full"
               ></iframe> */}
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/Xeans-2Hvdg?si=DOmtdqBPBxfGWq8B" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/4F5d8w6Yins?si=pW88JBvSfyo0d7Ln" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
             </div>
 
             <div className="flex flex-col ">
