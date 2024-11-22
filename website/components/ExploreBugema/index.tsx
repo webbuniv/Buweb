c22e16cb5d3dbfc712bcf58c1ac0cf211a824e38
AKATWIJUKA-ELIA <eliaakjtrnq@gmail.com>
2024-11-22 10:27:21 +0300

    Update index.tsx

--- website/components/ExploreBugema/index.tsx
+++ website/components/ExploreBugema/index.tsx
@@ -115,9 +115,8 @@ const Video = () => {
             className="w-full h-full">
           </iframe> */}
 
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/Oa7Ns-xD52I?si=xQF89hYr44zBIqZd" title="YouTube video player"
-      frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>      
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/VpN2Jljw3HU?si=pL5nUPq6TjiQsqix" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>     
       </div>
 
     <div className="flex flex-col ">
