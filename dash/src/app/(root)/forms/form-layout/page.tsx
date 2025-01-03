33306c020384396b6347ce2f993a0c02b99f8466
Daniel Ntege <danientege785@gmail.com>
2025-01-03 21:10:11 +0300

    news form

--- dash/src/app/(root)/forms/form-layout/page.tsx
+++ dash/src/app/(root)/forms/form-layout/page.tsx
@@ -26,7 +26,7 @@ const FormLayout = () => {
     };
   return (
     <>
-      <Breadcrumb pageName="FormLayout" />
+      <Breadcrumb pageName="Create News" />
 
         <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
           <div className="border-b border-stroke px-6.5 py-4 dark:border-strokedark">
