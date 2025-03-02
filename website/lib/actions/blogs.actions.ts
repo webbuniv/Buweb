e1de2baefe75544c5ae4b3e849febe77d19ea349
Daniel Ntege <danientege785@gmail.com>
2025-03-02 23:01:45 +0300

    Update API endpoint to fetch only published blogs and clean up file size formatting

--- website/lib/actions/blogs.actions.ts
+++ website/lib/actions/blogs.actions.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 export const getBlogs = async () => {
     try {
-        const response = await axios.get(`${API_URL}/posts`);
+        const response = await axios.get(`${API_URL}/posts/published`);
         return response.data;
     } catch (error) {
         throw new Error(error);
--- website/lib/utils.ts
+++ website/lib/utils.ts
@@ -11,10 +11,10 @@ export const convertFileToUrl = (file: File) => URL.createObjectURL(file);
 
 export const convertFileSize = (sizeInBytes: number, digits?: number) => {
   if (sizeInBytes < 1024) {
-    return sizeInBytes + " Bytes"; // Less than 1 KB, show in Bytes
+    return sizeInBytes + " Bytes";
   } else if (sizeInBytes < 1024 * 1024) {
     const sizeInKB = sizeInBytes / 1024;
-    return sizeInKB.toFixed(digits || 1) + " KB"; // Less than 1 MB, show in KB
+    return sizeInKB.toFixed(digits || 1) + " KB";
   } else if (sizeInBytes < 1024 * 1024 * 1024) {
     const sizeInMB = sizeInBytes / (1024 * 1024);
     return sizeInMB.toFixed(digits || 1) + " MB"; // Less than 1 GB, show in MB
