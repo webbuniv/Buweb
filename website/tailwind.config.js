0114208524a6653af5cad2d2738a57f82cd8408f
Daniel Ntege <danientege785@gmail.com>
2025-07-30 19:36:13 +0300

    commit

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,92 +1,85 @@
 /** @type {import('tailwindcss').Config} */
+const defaultConfig = require("shadcn/ui/tailwind-config")
+
 module.exports = {
-  content: [
-      "./node_modules/flowbite-react/lib/**/*.js",
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  darkMode: ["class", "class"],
+  ...defaultConfig,
+  content: [...defaultConfig.content, "./node_modules/flowbite-react/lib/**/*.js", "*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-  	container: {
-  		center: true,
-  		padding: '1rem'
-  	},
-  	colors: {
-  		current: 'currentColor',
-  		transparent: 'transparent',
-  		white: '#FFFFFF',
-  		black: '#090E34',
-  		dark: '#1D2144',
-  		primary: '#4A6CF7',
-  		yellow: '#FBB040',
-  		'body-color': '#959CB1'
-  	},
-  	screens: {
-  		xs: '450px',
-  		sm: '575px',
-  		md: '768px',
-  		lg: '992px',
-  		xl: '1200px',
-  		'2xl': '1400px'
-  	},
-  	extend: {
-  		boxShadow: {
-  			signUp: '0px 5px 10px rgba(4, 10, 34, 0.2)',
-  			one: '0px 2px 3px rgba(7, 7, 77, 0.05)',
-  			sticky: 'inset 0 -1px 0 0 rgba(0, 0, 0, 0.1)'
-  		},
-  		animation: {
-  			'shrink-out': 'shrinkOut 1s forwards',
-  			'grow-in': 'growIn 1s forwards',
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out'
-  		},
-  		keyframes: {
-  			shrinkOut: {
-  				'0%': {
-  					transform: 'scale(1)',
-  					opacity: '1'
-  				},
-  				'100%': {
-  					transform: 'scale(0)',
-  					opacity: '0'
-  				}
-  			},
-  			growIn: {
-  				'0%': {
-  					transform: 'scale(0)',
-  					opacity: '0'
-  				},
-  				'100%': {
-  					transform: 'scale(1)',
-  					opacity: '1'
-  				}
-  			},
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    ...defaultConfig.theme,
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    colors: {
+      ...defaultConfig.theme.extend.colors,
+      current: "currentColor",
+      transparent: "transparent",
+      white: "#FFFFFF",
+      black: "#090E34",
+      dark: "#1D2144",
+      yellow: "#FBB040",
+      "body-color": "#959CB1",
+    },
+    screens: {
+      xs: "450px",
+      sm: "575px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      "2xl": "1400px",
+    },
+    extend: {
+      ...defaultConfig.theme.extend,
+      boxShadow: {
+        signUp: "0px 5px 10px rgba(4, 10, 34, 0.2)",
+        one: "0px 2px 3px rgba(7, 7, 77, 0.05)",
+        sticky: "inset 0 -1px 0 0 rgba(0, 0, 0, 0.1)",
+      },
+      animation: {
+        "shrink-out": "shrinkOut 1s forwards",
+        "grow-in": "growIn 1s forwards",
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      keyframes: {
+        shrinkOut: {
+          "0%": {
+            transform: "scale(1)",
+            opacity: "1",
+          },
+          "100%": {
+            transform: "scale(0)",
+            opacity: "0",
+          },
+        },
+        growIn: {
+          "0%": {
+            transform: "scale(0)",
+            opacity: "0",
+          },
+          "100%": {
+            transform: "scale(1)",
+            opacity: "1",
+          },
+        },
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+      },
+    },
   },
-  plugins: [require("flowbite/plugin"), require("tailwindcss-animate")],
-};
+  plugins: [require("flowbite/plugin"), ...defaultConfig.plugins],
+}
